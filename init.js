28c16558fded58fc7f98bec83ad016908e8cf9c4
j <code@jtk3.xyz>
2018-10-18 18:29:38 -0700

    Update init.js

--- init.js
+++ init.js
@@ -3,3 +3,11 @@ dtps.log = function(msg) {
 console.log("[DTPS] " + msg);
 }
 dtps.log("It's alive!");
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     dtps.log(this.responseText);
+    }
+  };
+  xhttp.open("GET", "bookmark.txt", true);
+  xhttp.send();
