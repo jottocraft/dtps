71da7190d642f437867a42ae42587addafa8e3c5
j <code@jtk3.xyz>
2018-10-18 18:31:24 -0700

    Update init.js

--- init.js
+++ init.js
@@ -9,5 +9,5 @@ var xhttp = new XMLHttpRequest();
      dtps.log(this.responseText);
     }
   };
-  xhttp.open("GET", "bookmark.txt", true);
-  xhttp.send();
+  xhttp.open("POST", "https://dtechhs.learning.powerschool.com/u/10837719/portal/portlet_grades?my_portal=true", true);
+  xhttp.send("id=9915857+10145967+9715213+9915843+9915882&e=15668509&csrf_token=823f04b43cd4b367d8bd67070702a86983d29f4e");
