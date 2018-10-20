6c720429042ab246c84dc1e8aa2cec5f0cf0531e
jottocraft <jack@loefflerfamily.com>
2018-10-20 16:59:19 -0700

    Update init.js

--- init.js
+++ init.js
@@ -196,6 +196,13 @@ dtps.classStream = function(num) {
 	   dtps.classes[num].streamitems.push(assignment.find("a").attr("onclick").split("/")[5].replace("')", "")) 
         dtps.classes[num].streamlist.push(`
 <div class="card assignment">
+<div class="points">
+<div class="spinner">
+  <div class="bounce1"></div>
+  <div class="bounce2"></div>
+  <div class="bounce3"></div>
+</div>
+</div>
     <h4>` + assignment.children("td:nth-child(1)").text() + `</h4>
 	<h5>Due ` + due + `</h5>
 </div>
