4cfd43ef86a8a5253bb80e144ec0c35619a30eca
j <code@jtk3.xyz>
2018-10-20 17:23:35 -0700

    Update init.js

--- init.js
+++ init.js
@@ -214,8 +214,8 @@ dtps.classStream = function(num) {
 	     data = jQuery(this.responseText).find("table.list.hover_glow tbody").children("tr:not(.noglow):not(:has(th))").toArray();
 	    for (var i = 0; i < data.length; i++) {
 	    var id = dtps.classes[num].streamitems.indexOf(jQuery(data[i]).find("a").attr("href").split("/")[5])
-	    dtps.classes[num].streamitems[id].grade = jQuery(data[i]).children("td:nth-child(4)").text().replace(/\s/g, "");
-	    dtps.classes[num].streamitems[id].letter = jQuery(data[i]).children("td:nth-child(6)").text().replace(/\s/g, "");
+	    dtps.classes[num].stream[id].grade = jQuery(data[i]).children("td:nth-child(4)").text().replace(/\s/g, "");
+	    dtps.classes[num].stream[id].letter = jQuery(data[i]).children("td:nth-child(6)").text().replace(/\s/g, "");
 	    }
     }
   }    
