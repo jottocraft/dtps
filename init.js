7be1d88c97179942db743e22442b49b7e501e654
j <code@jtk3.xyz>
2018-10-21 21:17:29 -0700

    beta 0.1.0

--- init.js
+++ init.js
@@ -1,15 +1,17 @@
 var dtps = {
-  ver: 006,
-  readableVer: "v0.0.6 [ALPHA]",
-  trackSuffix: " [ALPHA]"
+  ver: 010,
+  readableVer: "v0.1.0 (Beta)",
+  trackSuffix: " (Beta)"
 };
 dtps.changelog = function () {
   jQuery("body").append(`<div id="TB_overlay" style="position: fixed;">&nbsp;</div><div id="TB_window" role="dialog" aria-modal="true" aria-labelledby="TB_title" style="width: 800px; height: 540px;margin: 100px calc(50% - 400px);"><div id="TB_closeAjaxWindow" class="tb_title_bar" role="heading"><a href="javascript:;" onclick="TB_remove();" id="TB_closeWindowButton" aria-hidden="true"><i class="icon-close"></i></a><div id="TB_title" class="tb_title">Project DTPS</div><div id="TB_ajaxContent" role="main" style="width: 770px; height: 434px;">
 <h2>What's new in Project DTPS</h2>
 <h4>` + dtps.readableVer + `</h4>
 <ul>
-<li>Added class stream phase one (PowerSchool seperates assignments into the all assignments category and graded assignments. So, to get assignment grades, I need to check the grades, but it leaves out ungraded assignments. Right now, it just shows all assignments, and grades are not reflected. Phase two will add grades. Stream phase 3 will add the stream for all classes combined)</li>
-<li>Next update bringing stream phase two, class colors, and beta release</li>
+<li>Promoted from alpha to beta</li>
+<li>Added class colors (fetched from PowerSchool)</li>
+<li><a href="https://chrome.google.com/webstore/detail/project-dtps/pakgdifknldaiglefmpkkgfjndemfapo/">Chrome Extension</a></li>
+<li>Up next: multi-block pages and stream. Gradebook is low priority right now, so I'll be hiding the tab until it's ready.</li>
 </ul>
 </div><div id="TB_actionBar" style=""><span><input class="button button" onclick="ThickBox.close();dtps.render();" type="button" value="Continue"></span>
 `)
@@ -21,13 +23,13 @@ dtps.firstrun = function () {
   jQuery("body").append(`<div id="TB_overlay" style="position: fixed;">&nbsp;</div><div id="TB_window" role="dialog" aria-modal="true" aria-labelledby="TB_title" style="width: 800px; height: 540px;margin: 100px calc(50% - 400px);"><div id="TB_closeAjaxWindow" class="tb_title_bar" role="heading"><a href="javascript:;" onclick="TB_remove();" id="TB_closeWindowButton" aria-hidden="true"><i class="icon-close"></i></a><div id="TB_title" class="tb_title">Project DTPS</div><div id="TB_ajaxContent" role="main" style="width: 770px; height: 434px;">
 <h2>Welcome to Project DTPS</h2>
 <h4>` + dtps.readableVer + `</h4>
-<h4>Before using project dtps, make sure you know what you're signing up for. Read all of this carefully.<h4>
+<h4>Before using Project DTPS, make sure you know what you're signing up for. Read all of this carefully.<h4>
 <li>Project dtps is a powerschool skin / mod that will pull data from your powerschool account and show you a much better UI</li>
 <li>Project dtps is meant to be simple, so various powerschool features will be left out. If you need to use an unsupported feature, reload powerschool and don't click the bookmark.</li>
-<li>Project dtps will not store any of your user data anywhere. Project dtps currently only saves the last used version of project dtps (for changelogs) and if you agree to this prompt locally on your computer as a cookie.</li>
+<li>Project dtps will not store any of your user data anywhere. Project dtps currently only saves the last used version of Project DTPS (for changelogs) and if you agree to this prompt locally on your computer as a cookie.</li>
 <li>Project dtps has the potential to write / edit data on your powerschool account, but there are no plans for that and it currently does not write/edit anything on your powerschool account, powerschool assignments, any powerschool coursework, etc.</li>
-<li>Project dtps needs to be launched via the bookmark <b>every time</b> you visit powerschool in order to work. You can choose to not click the bookmark and powerschool will work as normal. Project dtps does not and cannot run any scrips of any kind without you clicking the bookmark. A chrome extension may be created in the future to automatically load project dtps.</li>
-<li>To prevent any privacy incidents from occuring, please don't run other scripts alongside project dtps. project dtps can be reverse engineered and taken advantage of if you run other scripts.</li>
+<li>Project dtps needs to be launched via the bookmark <b>every time</b> you visit powerschool in order to work. You can choose to not click the bookmark and powerschool will work as normal. Project dtps does not and cannot run any scrips of any kind without you clicking the bookmark. A chrome extension may be created in the future to automatically load Project DTPS.</li>
+<li>To prevent any privacy incidents from occuring, please don't run other scripts alongside Project DTPS. Project DTPS can be reverse engineered and taken advantage of if you run other scripts.</li>
 <li><b>Project dtps or myself are not responsible for any issues of any kind regarding anything related to powerschool. Use this at your own risk. Project dtps will have bugs, some of which may inaccuratly reflect your academic status. If you have any doubts, you can just not click the bookmark and use powerschool normally.</b></li>
 </div><div id="TB_actionBar" style=""><span><input class="button button" onclick="ThickBox.close();" type="button" value="Cancel"><input class="button button" onclick="ThickBox.close(); document.cookie = 'dtpsInstalled=true'; dtps.render();" type="button" value="Accept & Continue"></span>
 `)
@@ -58,10 +60,12 @@ dtps.getCookie = function(cname) {
 }
 dtps.init = function () {
   dtps.log("Starting DTPS " + dtps.readableVer + "...");
+  var sudoers = ["10837719"]
+  if (sudoers.includes(HaikuContext.user.login)) { jQuery("body").addClass("dev"); dtps.log("Dev mode enabled") }
   dtps.shouldRender = false;
-  dtps.user = jQuery("#header-body h1").text().split(", ")[1];
+  dtps.user = HaikuContext.user;
   dtps.classColors = [];
-  var eClassList = jQuery(".eclass_list ul").toArray();
+  var eClassList = jQuery(".eclass_list ul").children().toArray();
   for (var i = 0; i < eClassList.length; i++) {
 	  var eclass = jQuery(eClassList[i])
 	  var col = jQuery.grep(eclass.attr("class").split(" "), function (item, index) {
@@ -69,7 +73,7 @@ dtps.init = function () {
           })[0].replace("border_color_", "filter_");
 	  var id = jQuery.grep(eclass.attr("class").split(" "), function (item, index) {
                return item.trim().match(/^eclass_/);
-          })[1];
+          })[1].replace("eclass_", "");
 	  dtps.classColors.push({id: id, col: col}); 
   }
   if (window.location.host !== "dtechhs.learning.powerschool.com") {
@@ -100,12 +104,18 @@ var xhttp = new XMLHttpRequest();
         var section = jQuery(data[i]);
         var grade = section.children(".right").text().replace(/\s/g, "").replace("%", "");
         var name = jQuery(section.children()[1]).text();
-        var loc = section.children("td").children("a.filter").attr("href").split("/")
+        var loc = section.children("td").children("a.filter").attr("href").split("/");
+		var id = jQuery.grep(section.children("td").children("a.filter").attr("class").split(" "), function (item, index) {
+               return item.trim().match(/^eclass_/);
+          })[0].replace("eclass_", "");
         var subject = null;
         if (name.includes("Physics")) { var subject = "Physics" }; if (name.includes("English")) { var subject = "English" }; if (name.includes("Physical Education")) { var subject = "PE" };
         if (name.includes("Prototyping")) { var subject = "Prototyping" }; if (name.includes("Algebra")) { var subject = "Algebra" };if (name.includes("Algebra 2")) { var subject = "Algebra 2" };
         if (name.includes("Spanish")) { var subject = "Spanish" }; if (name.includes("@") || name.includes("dtech")) { var subject = "@d.tech" };
         if (subject == null) var subject = name;
+		for (var ii = 0; ii < dtps.classColors.length; ii++) {
+		if (dtps.classColors[ii].id == id) var col = dtps.classColors[ii].col;
+		}
         dtps.classes.push({
           name: name,
           subject: subject,
@@ -113,6 +123,8 @@ var xhttp = new XMLHttpRequest();
           grade: (grade == "--") ? ( grade ) : (Number(grade.match(/\d+/g).join(".")).toFixed(2)),
           letter: (grade == "--") ? ( grade ) : (grade.replace(/[0-9]/g, '').replace(".", "")),
           loc: loc[1] + "/" + loc[2],
+		  col: col,
+		  id: id,
           num: i
         })
       }
@@ -310,15 +322,19 @@ dtps.getPage = function(loc, id) {
 dtps.showClasses = function () {
   var streamClass = "active"
   if (dtps.selectedClass !== "stream") var streamClass = "";
-  jQuery(".sidebar").html(`<div onclick="dtps.selectedClass = 'stream';" class="class ` + streamClass + `">
+  jQuery(".sidebar").html(`<div onclick="dtps.selectedClass = 'stream';" class="class ` + streamClass + ` dev">
 <div class="label">Stream</div>
-<div class="grade"><i class="material-icons">view_stream</i></div>
+<div class="grade"><i class="material-icons">code</i></div>
 </div>
-<div class="classDivider"></div>
+<div class="classDivider dev"></div>
 ` + dtps.classlist.join(""));
   if (dtps.selectedClass !== "stream") $(".class." + dtps.selectedClass).addClass("active");
   if ($(".btn.pages").hasClass("active")) { $(".btn.pages").removeClass("active"); $(".btn.stream").addClass("active"); dtps.classStream(dtps.selectedClass); dtps.selectedContent = "stream"; }
        $( ".class" ).click(function(event) {
+	  $(".background").removeClass(jQuery.grep($(".background").attr("class").split(" "), function (item, index) {
+              return item.trim().match(/^filter_/);
+          })[0]);
+		  if (dtps.selectedClass !== "stream") $(".background").addClass(dtps.classes[dtps.selectedClass].col)
   $(this).siblings().removeClass("active")
   $(this).addClass("active")
   if (dtps.selectedContent == "stream") dtps.classStream(dtps.selectedClass);
@@ -339,7 +355,7 @@ dtps.render = function() {
   dtps.classlist = [];
   for (var i = 0; i < dtps.classes.length; i++) {
     dtps.classlist.push(`
-<div onclick="dtps.selectedClass = ` + i + `" class="class ` + i + `">
+<div onclick="dtps.selectedClass = ` + i + `" class="class ` + i + ` ` + dtps.classes[i].col + `">
 <div class="label">` + dtps.classes[i].subject + `</div>
 <div class="grade"><span class="letter">` + dtps.classes[i].letter + `</span><span class="points">` + dtps.classes[i].grade + `%</span></div>
 </div>
@@ -350,7 +366,7 @@ dtps.render = function() {
 </div>
 <div class="background">
 <div class="header">
-<h1 id="headText">Stream</h1>
+<h1 id="headText">PowerSchool</h1>
 <div style="display: none;" class="btns row">
 <button onclick="dtps.selectedContent = 'stream'; dtps.classStream(dtps.selectedClass);" class="btn active stream">
 Stream
@@ -360,9 +376,9 @@ Stream
 Pages
 <i class="material-icons">list</i>
 </button>
-<button onclick="dtps.selectedContent = 'grades'" class="btn grades">
+<button onclick="dtps.selectedContent = 'grades'" class="btn grades dev">
 Gradebook
-<i class="material-icons">book</i>
+<i class="material-icons">code</i>
 </button>
 </div>
 <div class="classContent">
