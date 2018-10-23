3f3f3dbf55b7bd7bbfdfc0e61e4988fd6155a937
j <code@jtk3.xyz>
2018-10-23 14:38:10 -0700

    Update init.js

--- init.js
+++ init.js
@@ -114,7 +114,7 @@ dtps.webReq = function(req, url, callback) {
 }
 dtps.init = function () {
   dtps.log("Starting DTPS " + dtps.readableVer + "...");
-  var sudoers = ["10837719", "10838212"]
+  var sudoers = ["10837719", "10838212", "10894474"]
   if (sudoers.includes(HaikuContext.user.login)) { jQuery("body").addClass("dev"); dtps.log("Dev mode enabled") }
   dtps.shouldRender = false;
   dtps.user = HaikuContext.user;
