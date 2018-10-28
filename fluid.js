8db766de06633e6f4df98411cb7c71acfb07cb1c
jottocraft <jack@loefflerfamily.com>
2018-10-28 16:27:01 -0700

    Update fluid.js

--- fluid.js
+++ fluid.js
@@ -237,8 +237,6 @@ if (fluid.expBeh) { event.preventDefault(); fluid.bounceBack(event.target); }
 
 }
 
-$( document ).ready(fluid.init);
-
 $( window ).resize(function() {
   fluid.exitContextMenu(true);
 });
