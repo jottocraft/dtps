154fbc5e8b7ca4d7f16f39638bc5caf00468083a
j <code@jtk3.xyz>
2018-11-01 17:00:14 -0700

    re-add carter as sudoer
    testing gradebook tomorrow

--- init.js
+++ init.js
@@ -117,7 +117,7 @@ dtps.webReq = function(req, url, callback, q) {
 }
 dtps.init = function () {
   dtps.log("Starting DTPS " + dtps.readableVer + "...");
-  var sudoers = ["10837719", "10838212", "10463823"]
+  var sudoers = ["10837719", "10838212", "10894474", "10463823"]
   if (sudoers.includes(HaikuContext.user.login)) { jQuery("body").addClass("dev"); dtps.log("Dev mode enabled") }
   dtps.shouldRender = false;
   dtps.user = HaikuContext.user;
