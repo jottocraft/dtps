af5762f9f6cfda4e096611e3ac0568b7e8bdd2ef
j <code@jtk3.xyz>
2018-11-02 17:49:35 -0700

    i dont want to know where you live

--- init.js
+++ init.js
@@ -102,13 +102,12 @@ dtps.init = function () {
 	dtps.showChangelog = false;
   dtps.user = HaikuContext.user;
   dtps.classColors = [];
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('set', 'page', "https://dtechhs.learning.powerschool.com");
+jQuery.getScript("https://www.google-analytics.com/analytics.js", function() {
 ga('create', 'UA-105685403-3', 'auto');
+ga('set', 'anonymizeIp', true);
+ga('set', 'page', "https://dtechhs.learning.powerschool.com");
 ga('send', 'pageview');
+});
   if (window.location.host !== "dtechhs.learning.powerschool.com") {
     dtps.shouldRender = false;
     dtps.alert("Unsupported school", "Project DTPS only works at Design Tech High School");
