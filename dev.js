7ec94df4b7b8d278d121dd259647de57db1ddb37
jottocraft <jack@loefflerfamily.com>
2018-12-11 19:22:30 -0800

    add a lot more themes (dev w/ 2.9.5 final) v1.2.0

--- dev.js
+++ dev.js
@@ -144,6 +144,7 @@ window.dataLayer = window.dataLayer || [];
       dtps.showChangelog = true;
 	    //Load fluid JS modules early for changelogs
     $ = jQuery;
+	    fluidThemes = [ "midnight", "nitro", "aquatic", "rainbow" ];
       jQuery.getScript('https://dtps.js.org/fluid.js');
 	    dtps.shouldRender = true;
       dtps.alert("Loading...", "Updating to Power+ " + dtps.readableVer);
