8f2fd9bac07600926eaeeb6ee6ceb37314114cb0
jottocraft <jack@loefflerfamily.com>
2018-12-13 09:59:57 -0800

    fullcalendar libraries (v1.2.0) might not use this for final. dev rn

--- dev.js
+++ dev.js
@@ -135,6 +135,8 @@ window.dataLayer = window.dataLayer || [];
   });
 	
 });
+	jQuery.getScript("https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js")
+	jQuery.getScript("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js")
   if ((window.location.host !== "dtechhs.learning.powerschool.com") && ((window.location.host !== "mylearning.powerschool.com") || (HaikuContext.user.login.split(".")[0] !== "dtps"))) {
     dtps.shouldRender = false;
     dtps.alert("Unsupported school", "Power+ only works at Design Tech High School");
@@ -779,6 +781,11 @@ dtps.render = function() {
     rel: "stylesheet",
     type: "text/css",
     href: "https://dtps.js.org/fluid.css"
+  }).appendTo("head");
+  jQuery("<link/>", {
+    rel: "stylesheet",
+    type: "text/css",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css"
   }).appendTo("head");
   jQuery("<link/>", {
     rel: "stylesheet",
