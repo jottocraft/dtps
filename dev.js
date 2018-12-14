72b237d9b5df1ca6e429842a4995c516399ee199
jottocraft <jack@loefflerfamily.com>
2018-12-14 10:02:31 -0800

    dashboard for sudoers (v1.4.0 dev)

--- dev.js
+++ dev.js
@@ -661,10 +661,13 @@ dtps.showClasses = function() {
   }
 	if (!Boolean(jQuery(".sidebar .class.masterStream")[0])) {
   jQuery(".sidebar").html(`<h5 style="margin: 10px 0px 25px 0px; font-weight: 600; font-size: 27px; text-align: center;">Power+</h5>
-<div onclick="dtps.selectedClass = 'stream';" class="class masterStream ` + streamClass + `">
+` + { sudoers.includes(HaikuContext.user.login) ? (`<div onclick="dtps.selectedClass = 'stream';" class="class sudo masterStream ` + streamClass + `">
+    <div class="name">Dashboard</div>
+    <div class="grade"><i class="material-icons">dashboard</i></div>
+    </div>`) : ( `<div onclick="dtps.selectedClass = 'stream';" class="class masterStream ` + streamClass + `">
     <div class="name">Stream</div>
     <div class="grade"><i class="material-icons">view_stream</i></div>
-    </div>
+    </div>` )} + `
    <div onclick="dtps.selectedClass = 'announcements';" class="class">
     <div class="name" id="annLabel">Announcements ` + unreadAnn + `</div>
     <div class="grade"><i class="material-icons">announcement</i></div>
