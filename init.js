d75cb76993a89e52221d95205b390dfa07437899
jottocraft <jack@loefflerfamily.com>
2018-12-22 09:48:01 -0800

    fixes

--- init.js
+++ init.js
@@ -718,12 +718,12 @@ dtps.render = function() {
    if (e.detail == "extensionInstalled") {
 	        var extensionDom = "";
 	   jQuery(".extensionDom").html(`<br />
-<div id="extensionAutoLoad" onclick="$(this).toggleClass('active'); if (window.localStorage.disableAutoLoad) {localStorage.setItem('disableAutoLoad', false);} else {localStorage.setItem('disableAutoLoad', true); window.alert('Power+ Auto Load has been disabled. To re-enable, click on your profile picture at the top-right corner of PowerSchool and select the re-enable option in the dropdown menu.')}" class="switch active"><span class="head"></span></div>
+<div id="extensionAutoLoad" onclick="$(this).toggleClass('active'); if (window.localStorage.disableAutoLoad) {localStorage.setItem('disableAutoLoad', false); jQuery('#extensionDevMode').show();} else {localStorage.setItem('disableAutoLoad', true); jQuery('#extensionDevMode').hide();}" class="switch active"><span class="head"></span></div>
     <div class="label"><i class="material-icons">extension</i> Automatically load Power+</div>
 <br /><br />
 <div id="extensionDevMode" onclick="$(this).toggleClass('active'); if (window.localStorage.devAutoLoad) {localStorage.setItem('devAutoLoad', false);} else {localStorage.setItem('devAutoLoad', true);}" class="switch"><span class="head"></span></div>
     <div class="label"><i class="material-icons">extension</i> Load the dev (unstable) version of Power+</div>`)
-	   if (window.localStorage.disableAutoLoad == "true") jQuery("#extensionAutoLoad").removeClass("active");
+	   if (window.localStorage.disableAutoLoad == "true") jQuery("#extensionAutoLoad").removeClass("active"); jQuery("#extensionDevMode").hide();
 	   if (window.localStorage.devAutoLoad == "true") jQuery("#extensionDevMode").addClass("active");
    }
 });
