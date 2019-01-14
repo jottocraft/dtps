184d00372ed7ed27c825b9ded4bd3861e5274840
j <code@jtk3.xyz>
2019-01-14 10:51:28 -0800

    full class names icon

--- dev.js
+++ dev.js
@@ -1119,7 +1119,7 @@ dtps.render = function() {
     <div class="label"><i class="material-icons">font_download</i> Display letter grades instead of points earned</div>
     <br /><br />
     <div onclick="window.alert('coming soon')" class="switch sudo"><span class="head"></span></div>
-    <div class="label sudo"><i class="material-icons">experiment</i> Display full class names</div>
+    <div class="label sudo"><i class="material-icons">title</i> Display full class names</div>
     <br /><br />
     <div onclick="$('.gradeEditor').toggle();" class="switch sudo"><span class="head"></span></div>
     <div class="label sudo"><i class="material-icons">edit</i> Show grade editor (Power+ testers only)</div>
