ed4c3b4cece1311addfd65d4bbc56c7dfc22be39
jottocraft <hello@jottocraft.com>
2019-01-25 16:48:20 -0800

    dashboard settings placeholder

--- dev.js
+++ dev.js
@@ -1167,6 +1167,9 @@ dtps.render = function() {
     <div onclick="$('.abtpage').hide();$('.abtpage.classes').show();" class="item">
       <i class="material-icons">book</i> Classes
     </div>
+    <div onclick="$('.abtpage').hide();$('.abtpage.dashboard').show();" class="item sudo">
+      <i class="material-icons">widgets</i> Dashboard
+    </div>
     <div style="display:none;" onclick="$('.abtpage').hide();$('.abtpage.extension').show();" class="item extTab">
       <i class="material-icons">extension</i> Extension
     </div>
@@ -1220,6 +1223,10 @@ dtps.render = function() {
 <div style="display: none;" class="abtpage extension">
     <h5>Extension</h5>
     <div class="extensionDom" ></div>
+</div>
+<div style="display: none;" class="abtpage dashboard">
+    <h5>Dashboard</h5>
+    <p>Coming soon</p>
 </div>
 <div style="display: none;" class="abtpage experiments">
 <div class="sudo">
