f35642753a64d3b20cf7c29b5c33812fd28e2b54
j <code@jtk3.xyz>
2019-01-30 19:39:57 -0800

    update

--- dev.js
+++ dev.js
@@ -183,7 +183,7 @@ dtps.init = function () {
 jQuery.getScript('https://dtps.js.org/fluid.js', () => fluid.init);
 jQuery.getScript("https://www.googletagmanager.com/gtag/js?id=UA-105685403-3", function() {
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  gtag = function() {dataLayer.push(arguments);}
   var configTmp = {
   'page_title' : 'portal',
   'page_path': '/portal',
