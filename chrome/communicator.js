d396d6d8041ef89b002643d700b65240238eb8c4
j <code@jtk3.xyz>
2019-03-04 20:35:24 -0800

    Update communicator.js

--- chrome/communicator.js
+++ chrome/communicator.js
@@ -1,8 +1,9 @@
 if (window.location.pathname.split("/")[3] == "portal") {
 	
 	$(document).ready(function() {
-if ((window.localStorage.disableAutoLoad == undefined) || (window.localStorage.disableAutoLoad == "false")) {
-console.log("[DTPS Chrome] Automatically loading Project DTPS");
+		var url = 'https://dtps.js.org/init.js'
+if (window.localStorage.devAutoLoad == "true") var url = 'https://dtps.js.org/dev.js'
+
 window.dtpsLoader = 2;
 window.dtpsExtension = true;
 document.addEventListener('extensionData', function(e) {
@@ -12,12 +13,14 @@ document.addEventListener('extensionData', function(e) {
     }));
    }
 });
-var url = 'https://dtps.js.org/init.js'
-if (window.localStorage.devAutoLoad == "true") var url = 'https://dtps.js.org/dev.js'
+
+if ((window.localStorage.disableAutoLoad == undefined) || (window.localStorage.disableAutoLoad == "false")) {
+console.log("[DTPS Chrome] Automatically loading Project DTPS");
 $.getScript(url);
 } else {
-	jQuery("#myacc_dmenu").prepend(`<div class="dmoption" role="menuitem" taborder="-1" onclick="localStorage.setItem('disableAutoLoad', false); window.location.reload();"><a href="javascript:;" role="menuitem">Enable Power+</a></div>`)
+	jQuery("#global_top_links").append(`<a onclick="javascript:jQuery.getScript('` + url + `');dtpsLoader=2;">Power+` + (window.localStorage.devAutoLoad == "true" ? " (dev)" : "") + `</a>`)
 }
+
 	});
 	
 }
