30a49d582e11facf91fc0f12c70d3ce38b21efef
j <code@jtk3.xyz>
2020-01-15 19:42:54 -0800

    extension v2.1.6 source code

--- chrome/communicator.js
+++ chrome/communicator.js
@@ -1,106 +1,115 @@
-$(document).ready(function () {
-   console.log("[DTPS Chrome] Power+ is ready");
+if (window.location.hostname == "powerplus.app") {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(({ addedNodes }) => {
+      addedNodes.forEach(node => {
+        //Power+ button
+        if (node.nodeType === 1 && node.id == "dtpsInstallBtn") {
+          node.setAttribute("onclick", "window.location.href = 'https://dtechhs.instructure.com/power+'");
+          node.innerHTML = `<i class="material-icons">open_in_new</i> Open`;
+        }
+      })
+    })
+  })
 
-   //Canvas Script
-   var url = 'https://powerplus.app/init.js'
-   if (window.localStorage["pref-devChannel"] == "true") var url = (window.localStorage["pref-localDtps"] == "true" ? (window.localStorage.dtpsPath ? window.localStorage.dtpsPath : "http://localhost:8000/") + 'dev.js' : 'https://powerplus.app/dev.js')
+  // Starts the monitoring
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  })
+} else if (window.location.pathname == "/power+") {
 
-   window.dtpsLoader = 3;
+  //block most canvas scripts using yett
+  var s = document.createElement("script");
+  s.src = "https://cdn.jottocraft.com/canvas-blocker.min.js";
+  s.async = false;
+  document.documentElement.appendChild(s);
 
-   document.addEventListener('extensionData', function (e) {
-      if (e.detail == "extensionStatus") {
-         document.dispatchEvent(new CustomEvent('extensionData', {
-            detail: "extensionInstalled"
-         }));
-      }
-      if (e.detail == "extensionUninstall") {
-         chrome.runtime.sendMessage({ messageName: 'extensionUninstall' });
-      }
-   });
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(({ addedNodes }) => {
+      addedNodes.forEach(node => {
+        //Power+ preloader
+        if (node.nodeType === 1 && node.tagName === 'H1' && node.innerHTML.toUpperCase().includes("PAGE NOT FOUND")) {
+          node.innerHTML = "";
+          node.insertAdjacentHTML("afterend", `<div style="background-color: #191919; position: fixed; top: 0px; left: 0px; width: 100%; height: 100vh; z-index: 99;text-align: center;">
+          <img style="height: 115px; margin-top: 120px;" src="https://i.imgur.com/W2Di5X3.png" /><div class="loader"></div>
+          </div>`);
+          node.remove();
+
+          var s = document.createElement("style");
+          s.innerHTML = `body {background-color: #191919; overflow: hidden;}*,:after,:before{box-sizing:border-box}.loader{position:absolute;top:450px;left:50%;transform:translate(-50%,-50%);width:50px;height:10px;background:#3498db;border-radius:5px;animation:load 1.8s ease-in-out infinite}.loader:after,.loader:before{position:absolute;display:block;content:"";animation:load 1.8s ease-in-out infinite;height:10px;border-radius:5px}.loader:before{top:-20px;left:10px;width:40px;background:#ef4836}.loader:after{bottom:-20px;width:35px;background:#f5ab35}@keyframes load{0%{transform:translateX(40px)}50%{transform:translateX(-30px)}100%{transform:translateX(40px)}}`;
+          document.body.appendChild(s);
+
+          //tell Power+ the loader is already shown
+          var s = document.createElement("script");
+          s.textContent = "window.dtpsPreLoader = true;";
+          s.async = false;
+          document.documentElement.appendChild(s);
+        }
+        if (node.nodeType === 1 && node.tagName === 'HEADER' && node.id == "header") {
+          node.remove();
+        }
+        if (node.nodeType === 1 && node.tagName === 'DIV' && node.id == "wrapper") {
+          node.style = "margin: 0 !important;"
+        }
+        if (node.nodeType === 1 && node.tagName === 'TITLE') {
+          node.innerHTML = "Power+";
+        }
+        if (node.nodeType === 1 && node.tagName === 'LINK' && node.rel == "shortcut icon") {
+          node.type = "image/png"
+          node.href = "https://powerplus.app/favicon.png";
+        }
+        // For each added canvas link thing
+        if (node.nodeType === 1 && node.tagName === 'LINK' && node.type == "text/javascript") {
+          node.removeAttribute("as");
+          node.removeAttribute("rel");
+          node.type = "javascript/blocked";
+        }
+      })
+    })
+  })
+
+  // Starts the monitoring
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  })
+
+  //add jQuery
+  var s = document.createElement("script");
+  s.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+  s.async = false;
+  document.documentElement.appendChild(s);
 
 
+  //load Power+ script
+  window.dtpsLoader = 4;
+  var s = document.createElement("script");
+  s.src = window.localStorage.dtpsPath || "https://powerplus.app/init.js";
+  s.async = false;
+  document.documentElement.appendChild(s);
 
-   if (window.localStorage["pref-autoLoad"] == "true") {
-      $.getScript(url);
-   } else {
-      jQuery("#menu").append(`<li class="ic-app-header__menu-list-item">
-            <a onclick="jQuery.getScript('` + url + `');dtpsLoader=2;" style="cursor: pointer;" class="ic-app-header__menu-list-link">
+} else {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(({ addedNodes }) => {
+      addedNodes.forEach(node => {
+        //Power+ button
+        if (node.nodeType === 1 && node.id == "menu") {
+          node.insertAdjacentHTML("beforeend", `<li class="ic-app-header__menu-list-item">
+            <a href="/power+" style="cursor: pointer;" class="ic-app-header__menu-list-link">
               <div class="menu-item-icon-container" aria-hidden="true">
                   <img src="https://powerplus.app/whiteOutline.png" class="ic-icon-svg">
-
               </div>
-              <div class="menu-item__text" style="font-size: 14px;">Power+
-              ` + (window.localStorage["pref-devChannel"] == "true" ? `<div style="background-color: #ec9b06;color: white;font-size: 10px;line-height: 9px;vertical-align: middle;padding: 2px 2.5px;border-radius: 5px;display: inline-block;">dev</div>` : "") + `
-              </div>
+              <div class="menu-item__text" style="font-size: 14px;">Power+</div>
             </a>
           </li>`)
-		  
-		  if (window.localStorage["pref-devChannel"] == "true") {
-	jQuery(".feature-flags.collectionViewItems").prepend(`<li class="feature-flag"><div class="row-fluid feature dtps_dark">
-  <div class="span7">
-    <span class="element_toggler" aria-controls="dtps_dark-details" aria-expanded="false" role="button" tabindex="0">
-       <span class="screenreader-only">
-         Toggle feature details for Dark theme by Power+
-       </span>
-       <i class="icon-mini-arrow-right"></i>
-    </span>
-    <span class="feature-title">Dark theme by Power+ (beta)</span>
+        }
+      })
+    })
+  })
 
-    
-  </div>
-
-  <div class="span5 text-right">
-    
-      <!-- start super toggle -->
-      <label class="ic-Super-toggle--on-off" for="ff_toggle_dtps_dark">
-        <span class="screenreader-only">
-          
-            Enable feature: Dark theme by Power+
-          
-        </span>
-        <input onclick="if ($('#ff_toggle_dtps_dark').attr('checked') == 'checked') {window.localStorage['pref-darkCanvas'] = true;} else {window.localStorage['pref-darkCanvas'] = false;}" ` + (window.localStorage["pref-darkCanvas"] == "true" ? "checked" : "") + ` id="ff_toggle_dtps_dark" class="ic-Super-toggle__input ff_toggle" type="checkbox" data-id="dtps_dark">
-        <div class="ic-Super-toggle__container" aria-hidden="true" data-checked="On" data-unchecked="Off">
-          <div class="ic-Super-toggle__switch">
-            <div class="ic-Super-toggle__option--LEFT">
-              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="548.9" height="548.9" viewBox="0 0 548.9 548.9" xml:space="preserve">
-                <polygon points="449.3 48 195.5 301.8 99.5 205.9 0 305.4 95.9 401.4 195.5 500.9 295 401.4 548.9 147.5"></polygon>
-              </svg>
-            </div>
-            <div class="ic-Super-toggle__option--RIGHT">
-              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 28 28" xml:space="preserve">
-                <polygon points="28 22.4 19.6 14 28 5.6 22.4 0 14 8.4 5.6 0 0 5.6 8.4 14 0 22.4 5.6 28 14 19.6 22.4 28"></polygon>
-              </svg>
-            </div>
-          </div>
-        </div>
-      </label>
-      <!-- end super toggle -->
-    
-  </div>
-</div>
-
-<div class="feature-details row-fluid" id="dtps_dark-details" style="display: none;">
-  <div class="span10">
-    <p class="feature-description">
-      
-      
-        A dark theme for Canvas, made by Power+. Reload the page for changes to take effect. You can toggle this feature option here or in the settings menu of Power+, under the theme section.
-      
-    </p>
-  </div>
-
-  <div class="span2 text-right feature-detail-links">
-    
-  </div>
-</div>
-</li>`);
-   }
-
-   }
-});
-
-$(document.head).ready(function() {
-	if (!(window.localStorage["pref-autoLoad"] == "true") && (window.localStorage["pref-darkCanvas"] == "true") && (window.localStorage["pref-devChannel"] == "true")) {
-		javascript:jQuery("<link/>", { rel: "stylesheet", type: "text/css", href: "https://powerplus.app/canvasDark.css" }).appendTo("head");
-	}
-});
+  // Starts the monitoring
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  })
+}
