26f79fcaa1ee6536f7e15891cd0d545c669b3492
jottocraft <jotto3053@gmail.com>
2020-01-16 10:17:35 -0800

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,2 +1,3 @@
 //redirect to stable Power+
+alert("Power+ (dev) will no longer work starting on February 1st, 2020. Please update to the latest version of the Power+ chrome extension.");
 jQuery.getScript("https://powerplus.app/init.js");
