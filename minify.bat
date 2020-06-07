abff03080f58261c07d10378f722eb176b27e838
jottocraft <11508240+jottocraft@users.noreply.github.com>
2020-06-07 15:50:31 -0700

    minify init.js

--- minify.bat
+++ minify.bat
@@ -1,9 +1,21 @@
 @echo off
 
+mkdir output
+
+cd output
+
+mkdir lms
+
+cd ..
+
+cmd /q /c terser init.js --source-map -o output/init.js
+
 cd scripts
+
 forfiles /m *.js /c "cmd /q /c for %%I in (@fname) do terser %%~I.js --source-map -o ../output/%%~I.js"
 
 cd lms
+
 forfiles /m *.js /c "cmd /q /c for %%I in (@fname) do terser %%~I.js --source-map -o ../../output/lms/%%~I.js"
 
 cd ../..
