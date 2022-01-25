e8f8a7218bb79d42e5e729e67c238db5b378857c
j <code@jtk3.xyz>
2022-01-25 10:46:01 -0800

    fix cache

--- server.js
+++ server.js
@@ -36,38 +36,56 @@ server.route({
     method: "GET",
     path: "*"
 }, async ctx => {
+    var page;
     try {
-        const page = await getAssetFromKV(ctx.event);
-
-        //Store response
-        ctx.response.body = page.body;
-
-        //Set headers from KV
-        Object.assign(ctx.response.headers, Object.fromEntries(page.headers.entries()));
-
-        ctx.response.code = 200.1;
-
-        //Log entrypoint requests for analytics
-        const ref = ctx.request.headers.get("referer") && new URL(ctx.request.headers.get("referer"));
-        const isEntryPoint = ctx.url.pathname === "/init.js" || ctx.url.pathname.startsWith("/scripts/lms/");
-        const isOriginatingFromThirdParty = ref && !ref.hostname.endsWith("powerplus.app") && !ref.hostname.endsWith("jottocraft.com");
-        const isDtechFollowUp = (ref.hostname === "dtechhs.instructure.com") && (ctx.url.pathname === "/scripts/lms/canvas.js");
-
-        if (isEntryPoint && isOriginatingFromThirdParty && !isDtechFollowUp) {
-            //Record User-Agent, requested entrypoint, institution, country, state, and the Cloudflare colocation/datacenter handling the request
-            ctx.loggingEnabled = true;
-            ctx.loggingProperties = {
-                at: new Date().getTime(),
-                ua: ctx.request.headers.get("user-agent") || "unknown",
-                entry: ctx.url.pathname,
-                institution: ref.hostname,
-                country: ctx.event.request.cf.country,
-                region: ctx.event.request.cf.regionCode,
-                colo: ctx.event.request.cf.colo
-            };
-        }
+        page = await getAssetFromKV(ctx.event);
     } catch(e) {
-        ctx.response.code = 404.1;
+        return ctx.response.code = 404.1;
+    }
+
+    //Store response
+    ctx.response.body = page.body;
+
+    //Set headers from KV
+    Object.assign(ctx.response.headers, Object.fromEntries(page.headers.entries()));
+
+    ctx.response.code = page.status;
+    ctx.response.description = "No error; The asset corresponding to the request was successfully returned";
+
+    //Log entrypoint requests for analytics
+    const ref = ctx.request.headers.get("referer") && new URL(ctx.request.headers.get("referer"));
+    const isEntryPoint = ctx.url.pathname === "/init.js" || ctx.url.pathname.startsWith("/scripts/lms/");
+    const isOriginatingFromThirdParty = ref && !ref.hostname.endsWith("powerplus.app") && !ref.hostname.endsWith("jottocraft.com");
+    const isDtechFollowUp = (ref.hostname === "dtechhs.instructure.com") && (ctx.url.pathname === "/scripts/lms/canvas.js");
+
+    if (isEntryPoint && isOriginatingFromThirdParty && !isDtechFollowUp) {
+        const isInstructureHosted = ref.hostname.endsWith("instructure.com");
+        const isDtechStudent = ref.hostname === "dtechhs.instructure.com";
+        const onDNetwork = ctx.request.headers.get("cf-connecting-ip") === DTECH_IP;
+    
+        /**
+         * Record analytics data:
+         * - time
+         * - user agent
+         * - loaded entrypoint
+         * - is on instructure.com
+         * - is d.tech student
+         * - is on d.tech oracle network
+         * - country / region
+         * - cloudflare colo datacenter
+         */
+        ctx.loggingEnabled = true;
+        ctx.loggingProperties = {
+            at: new Date().getTime(),
+            ua: ctx.request.headers.get("user-agent") || "unknown",
+            entry: ctx.url.pathname,
+            isInstructureHosted: isInstructureHosted,
+            isDtechStudent: isDtechStudent,
+            onDNetwork: onDNetwork,
+            country: ctx.event.request.cf.country,
+            region: ctx.event.request.cf.regionCode,
+            colo: ctx.event.request.cf.colo
+        };
     }
 });
 
